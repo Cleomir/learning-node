93ba541ba4210fd3119bd05efca482f651b7f0df
Cleomir Machado Gomes <cleomir.mg@hotmail.com>
2024-03-03 16:11:49 -0300

    Add command-line arguments example

---
+++ command-line-arguments.js
@@ -0,0 +1,9 @@
+//  1. Run the file using node command
+//  2. Pass the arguments after the file name
+//  3. The arguments are stored in process.argv array
+//  4. process.argv[0] is the path to node
+//  5. process.argv[1] is the path to the file
+//  6. process.argv[2] is the first argument
+//  7. process.argv[3] is the second argument and so on
+//  e.g. node command-line-arguments.js arg1 arg2 arg3
+console.log(process.argv);
