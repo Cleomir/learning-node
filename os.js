d35acaec7ef3aee755fbf20449ff0479d0849f57
Cleomir Machado Gomes <cleomir.mg@hotmail.com>
2024-03-09 22:13:57 -0300

    Add availableParallelism function to os.js

--- os.js
+++ os.js
@@ -6,3 +6,5 @@ console.log(os.arch()); // 'x64'
 console.log(os.hostname()); // 'DESKTOP-123'
 // os type
 console.log(os.type()); // 'Linux'
+// number of threads available
+console.log(os.availableParallelism()); // 16
